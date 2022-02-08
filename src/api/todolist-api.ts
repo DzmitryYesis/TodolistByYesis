dd63c0239eb5ebc04406d66aeef8c7c638684a72
DzmitryYesis <yesis1988@mail.ru>
2022-02-08 15:25:52 +0300

    Step 18.8 Done Typing for GET and POST axios request

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -15,30 +15,50 @@ type TodolistType = {
     order: number
 }
 
-type ResponseTodoType<D = {}> = {
+type ResponseType<D = {}> = {
     fieldsErrors: Array<string>
     messages: Array<string>
     resultCode: number
     data: D
 }
 
+type TaskType = {
+    addedDate: string
+    deadline: string
+    description: string
+    id: string
+    order: number
+    priority: number
+    startDate: string
+    status: number
+    title: string
+    todoListId: string
+}
+
+type GetTaskType = {
+    totalCount: number,
+    error: string | null,
+    items: Array<TaskType>
+}
+
+
 export const todolistAPI = {
     getTodolists() {
         return instance.get<Array<TodolistType>>('todo-lists')
     },
     createTodolist(title: string) {
-        return instance.post<ResponseTodoType<{ item: TodolistType }>>('todo-lists', {title})
+        return instance.post<ResponseType<{ item: TodolistType }>>('todo-lists', {title})
     },
     deleteTodolist(todolistId: string) {
-        return instance.delete<ResponseTodoType>(`todo-lists/${todolistId}`)
+        return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
     },
     updateTodolistTitle(todolistId: string, title: string) {
-        return instance.put<ResponseTodoType>(`todo-lists/${todolistId}`, {title})
+        return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title})
     },
     getTasks(todolistId: string) {
-        return instance.get(`todo-lists/${todolistId}/tasks`)
+        return instance.get<GetTaskType>(`todo-lists/${todolistId}/tasks`)
     },
-    createTask(todolistId:string, title:string){
-        return instance.post(`todo-lists/${todolistId}/tasks`, {title})
+    createTask(todolistId: string, title: string) {
+        return instance.post<ResponseType<{ item: TaskType }>>(`todo-lists/${todolistId}/tasks`, {title})
     }
 }
