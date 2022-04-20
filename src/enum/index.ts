4dbdde208d599482facb6c969731cafb81466cb5
DzmitryYesis <yesis1988@mail.ru>
2022-04-20 20:18:37 +0300

    refactor: create enum ResultCode.ts

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -1,3 +1,5 @@
 export { PATH } from './path';
 
 export { RequestStatus } from './requestStatus';
+
+export { ResultCode } from './ResultCode';
---
+++ src/store/thunks/index.ts
@@ -0,0 +1,1 @@
+export { initializeAppTC, loginTC, logoutTC } from './authThunks';
