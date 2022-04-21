614340be285e8e2db09136b8290f0967ec63d060
DzmitryYesis <yesis1988@mail.ru>
2022-04-21 16:15:21 +0300

    refactor: formatting TodolistType.ts

--- src/types/TodolistType.ts
+++ src/types/TodolistType.ts
@@ -1,6 +1,14 @@
+import { FilterType } from 'store/reducers/todolists-reducer';
+import { RequestStatusType } from 'types';
+
 export type TodolistType = {
   id: string;
   title: string;
   addedDate: string;
   order: number;
+};
+
+export type TodolistDomainType = TodolistType & {
+  filter: FilterType;
+  entityStatus: RequestStatusType;
 };
