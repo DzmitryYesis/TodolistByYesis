29ad4b8113b9ee69b977329b94259ea3f089112f
DzmitryYesis <yesis1988@mail.ru>
2022-04-21 19:58:18 +0300

    refactor: add eslint and prettier rules for auth-api.ts

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,33 +1,16 @@
-import axios from 'axios';
-import {ResponseType} from './todolist-api';
-
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    withCredentials: true,
-    headers: {
-        'API-KEY': 'fdebf1e1-855a-49ea-8c4a-6136d43ccea5'
-    }
-})
-
-//api
+import { instance } from 'api';
+import { ResponseType, LoginParamsType } from 'types';
 
 export const authAPI = {
-    login(value: LoginParamsType) {
-        return instance.post<ResponseType<{userId:number}>>('auth/login', value)
-    },
-    me(){
-        return instance.get<ResponseType<{id:number, email:string, login:string}>>('auth/me')
-    },
-    logout(){
-        return instance.delete<ResponseType>('auth/login')
-    }
-}
-
-//types
-
-type LoginParamsType = {
-    email: string
-    password: string
-    rememberMe: boolean
-    captcha?: boolean
-}
+  login(value: LoginParamsType) {
+    return instance.post<ResponseType<{ userId: number }>>('auth/login', value);
+  },
+  me() {
+    return instance.get<ResponseType<{ id: number; email: string; login: string }>>(
+      'auth/me',
+    );
+  },
+  logout() {
+    return instance.delete<ResponseType>('auth/login');
+  },
+};
