6ff42eba915055def376af203a4c8528fa065b99
DzmitryYesis <yesis1988@mail.ru>
2022-04-21 15:59:22 +0300

    refactor: create enum TaskPriorities.ts

---
+++ src/enum/TaskPriorities.ts
@@ -0,0 +1,7 @@
+export enum TaskPriorities {
+  Low,
+  Middle,
+  Mi,
+  Urgently,
+  Later,
+}
--- src/enum/index.ts
+++ src/enum/index.ts
@@ -5,3 +5,5 @@ export { RequestStatus } from './requestStatus';
 export { ResultCode } from './ResultCode';
 
 export { TaskStatuses } from './TaskStatuses';
+
+export { TaskPriorities } from './TaskPriorities';
