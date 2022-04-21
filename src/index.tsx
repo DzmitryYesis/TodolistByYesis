b1332e11fa0fc3aa337109aa222b2809f633b428
DzmitryYesis <yesis1988@mail.ru>
2022-04-21 21:11:31 +0300

    refactor: add eslint and prettier rules for index.tsx, fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
-import App from './App';
-import {Provider} from 'react-redux';
-import {store} from './store/store';
-import {HashRouter} from 'react-router-dom';
+
+import { App } from 'App';
+import { store } from 'store/store';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <HashRouter>
-                <App/>
-            </HashRouter>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
