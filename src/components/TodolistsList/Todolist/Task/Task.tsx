521e46f5646c9aeb3da0d019e55f67b67cc71123
DzmitryYesis <yesis1988@mail.ru>
2022-04-22 16:22:21 +0300

    refactor: add eslint and prettier rules for Task.tsx. Fix bugs

--- src/components/TodolistsList/Todolist/Task/Task.tsx
+++ src/components/TodolistsList/Todolist/Task/Task.tsx
@@ -1,50 +1,73 @@
-import React, {ChangeEvent, useCallback} from 'react';
-import {Delete} from '@material-ui/icons';
-import {Checkbox, IconButton} from '@material-ui/core';
-import {TaskStatuses, TaskType} from './../../../../api/todolist-api';
-import {RequestStatusType} from './../../../../store/reducers/app-reducer';
-import SpanChangeTitle from 'components/SpanChangeTitle/SpanChangeTitle';
+import React, { ChangeEvent, useCallback } from 'react';
 
-type TaskComponentType = {
-    todolistId: string
-    task: TaskType
-    entityStatus: RequestStatusType
-    removeTask: (todolistId: string, taskId: string) => void
-    changeStatus: (todolistId: string, taskId: string, status: TaskStatuses) => void
-    changeTaskTitle: (todolistId: string, taskId: string, newTitle: string) => void
-}
+import { Checkbox, IconButton } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
 
-const Task = React.memo(({
-                             todolistId,
-                             task,
-                             entityStatus,
-                             removeTask,
-                             changeStatus,
-                             changeTaskTitle,
-                             ...props
-                         }: TaskComponentType) => {
-    console.log('Task')
-    const removeTaskHandler = useCallback(() => removeTask(todolistId, task.id), [removeTask, todolistId, task.id])
-    const changeStatusTasksHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-        let newIsDoneValue = e.currentTarget.checked
-        changeStatus(todolistId, task.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New)
-    }, [changeStatus, todolistId, task.id])
-    const functionForChangeTaskTitle = useCallback((newTitle: string) => {
-        changeTaskTitle(todolistId, task.id, newTitle)
-    }, [changeTaskTitle, todolistId, task.id])
+import { SpanChangeTitle } from 'components';
+import { RequestStatus, TaskStatuses } from 'enum';
+import { RequestStatusType, TaskType } from 'types';
+
+type TaskComponentPropsType = {
+  todolistId: string;
+  task: TaskType;
+  entityStatus: RequestStatusType;
+  removeTask: (todolistId: string, taskId: string) => void;
+  changeStatus: (todolistId: string, taskId: string, status: TaskStatuses) => void;
+  changeTaskTitle: (todolistId: string, taskId: string, newTitle: string) => void;
+};
+
+export const Task = React.memo(
+  ({
+    todolistId,
+    task,
+    entityStatus,
+    removeTask,
+    changeStatus,
+    changeTaskTitle,
+  }: TaskComponentPropsType) => {
+    const removeTaskHandler = useCallback(
+      () => removeTask(todolistId, task.id),
+      [removeTask, todolistId, task.id],
+    );
+
+    const changeStatusTasksHandler = useCallback(
+      (e: ChangeEvent<HTMLInputElement>) => {
+        const newIsDoneValue = e.currentTarget.checked;
+        changeStatus(
+          todolistId,
+          task.id,
+          newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New,
+        );
+      },
+      [changeStatus, todolistId, task.id],
+    );
+
+    const functionForChangeTaskTitle = useCallback(
+      (newTitle: string) => {
+        changeTaskTitle(todolistId, task.id, newTitle);
+      },
+      [changeTaskTitle, todolistId, task.id],
+    );
+
     return (
-        <div key={task.id} className={task.status === TaskStatuses.Completed ? 'is-done' : ''}>
-            <Checkbox
-                checked={task.status === TaskStatuses.Completed}
-                onChange={changeStatusTasksHandler}
-                color={'primary'}
-                disabled={entityStatus === 'loading'}
-            />
-            <SpanChangeTitle title={task.title} onChange={functionForChangeTaskTitle}/>
-            <IconButton onClick={removeTaskHandler} disabled={entityStatus === 'loading'}>
-                <Delete/>
-            </IconButton>
-        </div>)
-});
-
-export default Task;
+      <div
+        key={task.id}
+        className={task.status === TaskStatuses.Completed ? 'is-done' : ''}
+      >
+        <Checkbox
+          checked={task.status === TaskStatuses.Completed}
+          onChange={changeStatusTasksHandler}
+          color="primary"
+          disabled={entityStatus === RequestStatus.LOADING}
+        />
+        <SpanChangeTitle title={task.title} onChange={functionForChangeTaskTitle} />
+        <IconButton
+          onClick={removeTaskHandler}
+          disabled={entityStatus === RequestStatus.LOADING}
+        >
+          <Delete />
+        </IconButton>
+      </div>
+    );
+  },
+);
