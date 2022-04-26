2e566a8cd92d0017f68ddb1d5509890890d519bf
DzmitryYesis <yesis1988@mail.ru>
2022-04-26 17:02:05 +0300

    refactor: add eslint and prettier rules for Todolists.tsx.tsx. Fix bugs

--- src/components/TodolistsList/Todolist/Todolists.tsx
+++ src/components/TodolistsList/Todolist/Todolists.tsx
@@ -1,116 +1,144 @@
-import React, {useCallback, useEffect} from 'react';
-import InputForAdd from 'components/InputForAdd/InputForAdd';
-import SpanChangeTitle from 'components/SpanChangeTitle/SpanChangeTitle';
-import Task from './Task/Task';
-import {TaskStatuses, TaskType} from './../../../api/todolist-api';
-import {FilterType} from 'store/reducers/todolists-reducer';
-import {useDispatch} from 'react-redux';
-import {setTasksTC} from './../../../store/reducers/task-reducer';
-import {Button, ButtonGroup, IconButton} from '@material-ui/core';
-import {Delete} from '@material-ui/icons';
-import {RequestStatusType} from './../../../store/reducers/app-reducer';
+import React, { useCallback, useEffect } from 'react';
 
+import { Button, ButtonGroup, IconButton } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
+import { useDispatch } from 'react-redux';
+
+import { Task } from './Task';
+
+import { InputForAdd, SpanChangeTitle } from 'components';
+import { Filter, RequestStatus, TaskStatuses } from 'enum';
+import { setTasksTC } from 'store/thunks';
+import { FilterType, RequestStatusType, TaskType } from 'types';
 
 type TodolistType = {
-    key: string
-    todolistId: string
-    todolistTitle: string
-    tasks: Array<TaskType>
-    filter: FilterType
-    entityStatus: RequestStatusType
-    removeTask: (todolistId: string, taskId: string) => void
-    changeFilter: (todolistId: string, value: FilterType) => void
-    addTask: (todolistId: string, newTitle: string) => void
-    changeStatus: (todolistId: string, taskId: string, status: TaskStatuses) => void
-    removeTodolist: (todolistId: string) => void
-    addTodolist: (newTitle: string) => void
-    changeTodolistTitle: (todolistId: string, newTitle: string) => void
-    changeTaskTitle: (todolistId: string, taskId: string, newTitle: string) => void
-}
+  todolistId: string;
+  todolistTitle: string;
+  tasks: TaskType[];
+  filter: FilterType;
+  entityStatus: RequestStatusType;
+  removeTask: (todolistId: string, taskId: string) => void;
+  changeFilter: (todolistId: string, value: FilterType) => void;
+  addTask: (todolistId: string, newTitle: string) => void;
+  changeStatus: (todolistId: string, taskId: string, status: TaskStatuses) => void;
+  removeTodolist: (todolistId: string) => void;
+  changeTodolistTitle: (todolistId: string, newTitle: string) => void;
+  changeTaskTitle: (todolistId: string, taskId: string, newTitle: string) => void;
+};
 
-export const Todolists = React.memo(({
-                                         todolistId,
-                                         todolistTitle,
-                                         tasks,
-                                         filter,
-                                         entityStatus,
-                                         removeTask,
-                                         changeFilter,
-                                         addTask,
-                                         changeStatus,
-                                         removeTodolist,
-                                         addTodolist,
-                                         changeTodolistTitle,
-                                         changeTaskTitle,
-                                         ...props
-                                     }: TodolistType) => {
-    console.log('Todolist')
+export const Todolists = React.memo(
+  ({
+    todolistId,
+    todolistTitle,
+    tasks,
+    filter,
+    entityStatus,
+    removeTask,
+    changeFilter,
+    addTask,
+    changeStatus,
+    removeTodolist,
+    changeTodolistTitle,
+    changeTaskTitle,
+  }: TodolistType) => {
+    const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
     useEffect(() => {
-        dispatch(setTasksTC(todolistId))
-    }, [])
+      dispatch(setTasksTC(todolistId));
+    }, []);
 
-    const filterAll = useCallback(() => changeFilter(todolistId, 'all'), [changeFilter, todolistId])
-    const filterActive = useCallback(() => changeFilter(todolistId, 'active'), [changeFilter, todolistId])
-    const filterCompleted = useCallback(() => changeFilter(todolistId, 'completed'), [changeFilter, todolistId])
-    const removeTodolistHandler = () => removeTodolist(todolistId)
-    const functionForAddTask = useCallback((title: string) => {
-        addTask(todolistId, title)
-    }, [addTask, todolistId])
-    const functionForChangeTodolistTitle = useCallback((newTitle: string) => {
-        changeTodolistTitle(todolistId, newTitle)
-    }, [changeTodolistTitle, todolistId])
+    const filterAll = useCallback(
+      () => changeFilter(todolistId, Filter.ALL),
+      [changeFilter, todolistId],
+    );
+    const filterActive = useCallback(
+      () => changeFilter(todolistId, Filter.ACTIVE),
+      [changeFilter, todolistId],
+    );
+    const filterCompleted = useCallback(
+      () => changeFilter(todolistId, Filter.COMPLETED),
+      [changeFilter, todolistId],
+    );
 
+    const removeTodolistHandler = (): void => removeTodolist(todolistId);
 
-    if (filter === 'active') {
-        tasks = tasks.filter(t => t.status === TaskStatuses.New)
+    const functionForAddTask = useCallback(
+      (title: string) => {
+        addTask(todolistId, title);
+      },
+      [addTask, todolistId],
+    );
+
+    const functionForChangeTodolistTitle = useCallback(
+      (newTitle: string) => {
+        changeTodolistTitle(todolistId, newTitle);
+      },
+      [changeTodolistTitle, todolistId],
+    );
+
+    if (filter === Filter.ACTIVE) {
+      // eslint-disable-next-line no-param-reassign
+      tasks = tasks.filter(t => t.status === TaskStatuses.New);
     }
-    if (filter === 'completed') {
-        tasks = tasks.filter(t => t.status === TaskStatuses.Completed)
+    if (filter === Filter.COMPLETED) {
+      // eslint-disable-next-line no-param-reassign
+      tasks = tasks.filter(t => t.status === TaskStatuses.Completed);
     }
-
 
     return (
+      <div>
+        <h3>
+          <SpanChangeTitle
+            title={todolistTitle}
+            onChange={functionForChangeTodolistTitle}
+          />
+          <IconButton
+            onClick={removeTodolistHandler}
+            disabled={entityStatus === RequestStatus.LOADING}
+          >
+            <Delete />
+          </IconButton>
+        </h3>
+        <InputForAdd item={functionForAddTask} entityStatus={entityStatus} />
         <div>
-            <h3>
-                <SpanChangeTitle title={todolistTitle} onChange={functionForChangeTodolistTitle}/>
-                <IconButton onClick={removeTodolistHandler} disabled={entityStatus === 'loading'}>
-                    <Delete/>
-                </IconButton>
-            </h3>
-            <InputForAdd item={functionForAddTask} entityStatus={entityStatus}/>
-            <div>
-                {
-                    tasks.map(t => {
-                        return (
-                            <Task key={t.id}
-                                  todolistId={todolistId}
-                                  task={t}
-                                  removeTask={removeTask}
-                                  changeStatus={changeStatus}
-                                  changeTaskTitle={changeTaskTitle}
-                                  entityStatus={entityStatus}
-                            />
-                        )
-                    })
-                }
-            </div>
-            <div>
-                <ButtonGroup variant="text" aria-label="text button group">
-                    <Button onClick={filterAll}
-                            color={'default'}
-                            variant={filter === 'all' ? 'outlined' : 'text'}>All</Button>
-                    <Button onClick={filterActive}
-                            color={'primary'}
-                            variant={filter === 'active' ? 'outlined' : 'text'}>Active</Button>
-                    <Button onClick={filterCompleted}
-                            color={'secondary'}
-                            variant={filter === 'completed' ? 'outlined' : 'text'}>Completed</Button>
-                </ButtonGroup>
-            </div>
+          {tasks.map(t => (
+            <Task
+              key={t.id}
+              todolistId={todolistId}
+              task={t}
+              removeTask={removeTask}
+              changeStatus={changeStatus}
+              changeTaskTitle={changeTaskTitle}
+              entityStatus={entityStatus}
+            />
+          ))}
         </div>
+        <div>
+          <ButtonGroup variant="text" aria-label="text button group">
+            <Button
+              onClick={filterAll}
+              color="default"
+              variant={filter === Filter.ALL ? 'outlined' : 'text'}
+            >
+              All
+            </Button>
+            <Button
+              onClick={filterActive}
+              color="primary"
+              variant={filter === Filter.ACTIVE ? 'outlined' : 'text'}
+            >
+              Active
+            </Button>
+            <Button
+              onClick={filterCompleted}
+              color="secondary"
+              variant={filter === Filter.COMPLETED ? 'outlined' : 'text'}
+            >
+              Completed
+            </Button>
+          </ButtonGroup>
+        </div>
+      </div>
     );
-});
-
-export default Todolists;
+  },
+);
