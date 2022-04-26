d7b30c9dba7adf0c33e54856d27d865f39e4d102
DzmitryYesis <yesis1988@mail.ru>
2022-04-26 17:39:32 +0300

    refactor: fix import path for TodolistsList.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,7 @@ export { ErrorSnackbar } from './ErrorSnackbar';
 export { InputForAdd } from './InputForAdd';
 
 export { SpanChangeTitle } from './SpanChangeTitle';
+
+export { TodolistsList } from './TodolistsList';
+
+export { Login } from './Login';
--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -1,3 +1,7 @@
 export { selectIsLoggedIn, selectIsInitialized } from './selectAuth';
 
 export { selectStatus, selectError } from './selectApp';
+
+export { selectTasks } from './selectTasks';
+
+export { selectTodolists } from './selectTodolist';
